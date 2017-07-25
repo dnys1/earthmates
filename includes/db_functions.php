cccab5c1f2e155a2f217ac162d0771b147a47617
dnys1 <dnys1@asu.edu>
2017-07-25 09:53:32 -0700

    Removed sensitive data

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -4,34 +4,10 @@ declare(strict_types = 1);
 
 /* Ensure the session has started, i.e. load vars */
 require_once('session_start.php');
+/* Sensitive login data */
+require_once('db_login.php');
 
 $link = connectToDB();
-
-function connectToDB()
-{
-	try{
-			// Create a new connection.
-			// You'll probably want to replace hostname with localhost in the first parameter.
-			// Note how we declare the charset to be utf8mb4.  This alerts the connection that we'll be passing UTF-8 data.  This may not be required depending on your configuration, but it'll save you headaches down the road if you're trying to store Unicode strings in your database.  See "Gotchas".
-			// The PDO options we pass do the following:
-			// \PDO::ATTR_ERRMODE enables exceptions for errors.  This is optional but can be handy.
-			// \PDO::ATTR_PERSISTENT disables persistent connections, which can cause concurrency issues in certain cases.  See "Gotchas".
-			$link = new \PDO(   'mysql:host=localhost;dbname=id2197103_users;charset=utf8mb4',
-													'id2197103_usersroot',
-													'usersroot',
-													array(
-															\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-															\PDO::ATTR_PERSISTENT => false
-													)
-											);
-			
-			return $link;
-	}
-	catch(\PDOException $ex){
-			print($ex->getMessage());
-			return NULL;
-	}
-}
 
 function getAllCompetencies()
 {
